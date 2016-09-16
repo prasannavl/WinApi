1ae7e6e9a4b2a0c5a78788bbbf6008d02af7d2fe
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-09-16 16:51:24 +0530

    enhance: InvalidateRect IntPtr to ref Rectangle

--- User32/Methods.cs
+++ User32/Methods.cs
@@ -53,7 +53,7 @@ namespace WinApi.User32
         public static extern int ValidateRect(IntPtr hWnd, [In] ref Rectangle lpRect);
 
         [DllImport(LibraryName)]
-        public static extern int InvalidateRect(IntPtr hWnd, IntPtr lpRect, bool bErase);
+        public static extern int InvalidateRect(IntPtr hWnd, [In] ref Rectangle lpRect, bool bErase);
 
         [DllImport(LibraryName)]
         public static extern int ClipCursor(ref Rectangle rect);
