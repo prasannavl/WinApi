924047aa80d0918516dc5a21d52cdea44be12f0d
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-09-19 09:03:52 +0530

    enhace: implement object basics for Point, Size

--- WinApi/Core/Types.cs
+++ WinApi/Core/Types.cs
@@ -7,13 +7,82 @@ namespace WinApi.Core
     [StructLayout(LayoutKind.Sequential)]
     public struct Point
     {
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public bool Equals(Point other)
+        {
+            return (X == other.X) && (Y == other.Y);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is Point && Equals((Point) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (X*397) ^ Y;
+            }
+        }
+
         public int X, Y;
+
+        public static bool operator ==(Point left, Point right)
+        {
+            return (left.X == right.X) && (left.Y == right.Y);
+        }
+
+        public static bool operator !=(Point left, Point right)
+        {
+            return !(left == right);
+        }
     }
 
     [StructLayout(LayoutKind.Sequential)]
     public struct Size
     {
+        public Size(int width, int height) {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public bool Equals(Size other)
+        {
+            return (Width == other.Width) && (Height == other.Height);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is Size && Equals((Size) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Width*397) ^ Height;
+            }
+        }
+
         public int Width;
         public int Height;
+
+        public static bool operator ==(Size left, Size right)
+        {
+            return (left.Width == right.Width) && (left.Height == right.Height);
+        }
+
+        public static bool operator !=(Size left, Size right)
+        {
+            return !(left == right);
+        }
     }
 }
