5d2561f70afd49bebd4c9e5d57378f6b831bfd7a
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-09-19 14:56:29 +0530

    add: introduce D2DResources

---
+++ Samples/Sample.DirectX/D2DResources.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.DirectX
+{
+    class D2DResources : D3DResources
+    {
+
+    }
+}
