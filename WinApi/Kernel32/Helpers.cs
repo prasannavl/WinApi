b08c2436e42218103f9388318d5e0bd39dd51b38
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-09-20 13:20:32 +0530

    add: kernel32 helper

---
+++ WinApi/Kernel32/Helpers.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using WinApi.Extensions;
+
+// ReSharper disable InconsistentNaming
+
+namespace WinApi.Kernel32
+{
+    public static class Kernel32Helpers
+    {
+        public static Version GetVersion()
+        {
+            var dwVersion = Kernel32Methods.GetVersion();
+            var build = dwVersion < 0x80000000 ? dwVersion.HighAsInt() : 0; // (DWORD) (HIWORD(dwVersion))
+            var v = new Version(
+                (byte) dwVersion.Low(), // (DWORD)(LOBYTE(LOWORD(dwVersion)))
+                dwVersion.Low() >> 8 & 0xff,
+                (int) build // (DWORD)(HIBYTE(LOWORD(dwVersion)))
+            );
+            return v;
+        }
+
+        public static bool GetIsProcessorAMD64()
+        {
+            SystemInfo info;
+            Kernel32Methods.GetNativeSystemInfo(out info);
+            return info.ProcessorArchitecture == (uint) ProcessArchitecture.PROCESSOR_ARCHITECTURE_AMD64;
+        }
+
+        public static class ProcessUserModeExceptionFilter
+        {
+            const uint PROCESS_CALLBACK_FILTER_ENABLED = 0x1;
+
+            [DllImport(Kernel32Methods.LibraryName)]
+            public static extern int SetProcessUserModeExceptionPolicy(uint dwFlags);
+
+            [DllImport(Kernel32Methods.LibraryName)]
+            public static extern int GetProcessUserModeExceptionPolicy(out uint dwFlags);
+
+            private static bool IsApiAvailable()
+            {
+                if (GetIsProcessorAMD64())
+                {
+                    var ver = GetVersion();
+                    return ver.Major == 6 && ver.Minor == 1 && ver.Build > 7601;
+                }
+                return false;
+            }
+
+            public static bool Disable()
+            {
+                if (IsApiAvailable())
+                {
+                    uint dwFlags;
+                    if (GetProcessUserModeExceptionPolicy(out dwFlags) != 0)
+                    {
+                        // Turn off the bit
+                        dwFlags &= ~PROCESS_CALLBACK_FILTER_ENABLED;
+                        return SetProcessUserModeExceptionPolicy(dwFlags) != 0;
+                    }
+                }
+                return false;
+            }
+
+            public static bool Enable()
+            {
+                if (IsApiAvailable())
+                {
+                    uint dwFlags;
+                    if (GetProcessUserModeExceptionPolicy(out dwFlags) != 0)
+                    {
+                        // Turn off the bit
+                        dwFlags |= PROCESS_CALLBACK_FILTER_ENABLED;
+                        return SetProcessUserModeExceptionPolicy(dwFlags) != 0;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+}
