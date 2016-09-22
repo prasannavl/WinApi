b7299740f948fa9ded0d181358e9120a07707701
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-09-22 13:18:08 +0530

    add: WinApi.Desktop project

---
+++ WinApi.Desktop/ApplicationHelpers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using WinApi.XWin;
+
+namespace WinApi.Desktop
+{
+    public static class ApplicationHelpers
+    {
+        public static void InitializeCriticalErrorDisplay()
+        {
+            AppDomain.CurrentDomain.UnhandledException +=
+                (sender, eventArgs) =>
+                {
+                    ShowCriticalError(eventArgs.ExceptionObject);
+                };
+
+            WindowCoreBase.UnhandledException +=
+                (ref WindowException ex) =>
+                {
+                    ShowCriticalError(ex.DispatchedException);
+                };
+        }
+
+        public static void ShowCriticalError(object exceptionObj, string message = null, string title = null)
+        {
+            const string defaultMessage = "Oh snap! Something went wrong.";
+            if (message == null) message = defaultMessage;
+
+            ConsoleHelpers.EnsureConsole();
+            if (!string.IsNullOrEmpty(title)) Console.Title = title;
+            var exception = exceptionObj as Exception;
+            string msg;
+            if (exception != null)
+            {
+                msg = $"{Environment.NewLine}[CRITICAL] {message}{Environment.NewLine}{Environment.NewLine}" +
+                      $"{exception.Message}{Environment.NewLine}" +
+                      $"{exception.StackTrace}";
+            }
+            else
+            {
+                msg = $"{Environment.NewLine}[CRITICAL] {message}{Environment.NewLine}{Environment.NewLine}" +
+                      $"{exceptionObj.ToString()}{Environment.NewLine}";
+            }
+            Console.Error.WriteLine(msg);
+        }
+
+        public static void ShowCriticalInfo(string message, string title = null, bool suffixNewLine = true)
+        {
+            ConsoleHelpers.EnsureConsole();
+            if (!string.IsNullOrEmpty(title)) Console.Title = title;
+            if (suffixNewLine) Console.Error.WriteLine(message);
+            else Console.Error.Write(message);
+        }
+    }
+}
---
+++ WinApi.Desktop/ConsoleHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using WinApi.Kernel32;
+
+namespace WinApi.Desktop
+{
+    public static class ConsoleHelpers
+    {
+        public static bool EnsureConsole()
+        {
+            if (Kernel32Methods.GetConsoleWindow() != IntPtr.Zero) return true;
+            if (Kernel32Methods.AllocConsole() == 0) return false;
+            Console.SetOut(new StreamWriter(Console.OpenStandardOutput()) {AutoFlush = true});
+            Console.SetError(new StreamWriter(Console.OpenStandardOutput()) {AutoFlush = true});
+            Console.SetIn(new StreamReader(Console.OpenStandardInput()));
+            return true;
+        }
+    }
+}
