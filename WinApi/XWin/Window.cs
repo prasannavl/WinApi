43b3d009fd721dec23e5ca58577381776545d03a
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-09-22 20:26:00 +0530

    add: mouse input handling methods

--- WinApi/XWin/Window.cs
+++ WinApi/XWin/Window.cs
@@ -5,6 +5,29 @@ using WinApi.Extensions;
 
 namespace WinApi.XWin
 {
+    public enum MouseButton
+    {
+        Left = 0x1,
+        Right = 0x2,
+        Middle = 0x4,
+        XButton1 = 0x8,
+        XButton2 = 0x10,
+        Other = XButton1 | XButton2
+    }
+
+    public enum MouseButtonEvent
+    {
+        Up,
+        Down,
+        DoubleClick
+    }
+
+    public enum Orientation
+    {
+        Horizontal,
+        Vertical
+    }
+
     public abstract class WindowBase : WindowCoreBase
     {
         protected override void OnMessage(ref WindowMessage msg)
@@ -71,6 +94,95 @@ namespace WinApi.XWin
                     MessageHandlers.ProcessMouseMove(this, ref msg);
                     break;
                 }
+                case WM.LBUTTONUP:
+                {
+                    MessageHandlers.ProcessMouseButtonEvent(this, ref msg, MouseButton.Left, MouseButtonEvent.Up);
+                    break;
+                }
+                case WM.LBUTTONDOWN:
+                {
+                    MessageHandlers.ProcessMouseButtonEvent(this, ref msg, MouseButton.Left, MouseButtonEvent.Down);
+                    break;
+                }
+                case WM.LBUTTONDBLCLK:
+                {
+                    MessageHandlers.ProcessMouseButtonEvent(this, ref msg, MouseButton.Left,
+                        MouseButtonEvent.DoubleClick);
+                    break;
+                }
+                case WM.RBUTTONUP:
+                {
+                    MessageHandlers.ProcessMouseButtonEvent(this, ref msg, MouseButton.Right, MouseButtonEvent.Up);
+                    break;
+                }
+                case WM.RBUTTONDOWN:
+                {
+                    MessageHandlers.ProcessMouseButtonEvent(this, ref msg, MouseButton.Right, MouseButtonEvent.Down);
+                    break;
+                }
+                case WM.RBUTTONDBLCLK:
+                {
+                    MessageHandlers.ProcessMouseButtonEvent(this, ref msg, MouseButton.Right,
+                        MouseButtonEvent.DoubleClick);
+                    break;
+                }
+                case WM.MBUTTONUP:
+                {
+                    MessageHandlers.ProcessMouseButtonEvent(this, ref msg, MouseButton.Middle, MouseButtonEvent.Up);
+                    break;
+                }
+                case WM.MBUTTONDOWN:
+                {
+                    MessageHandlers.ProcessMouseButtonEvent(this, ref msg, MouseButton.Middle, MouseButtonEvent.Down);
+                    break;
+                }
+                case WM.MBUTTONDBLCLK:
+                {
+                    MessageHandlers.ProcessMouseButtonEvent(this, ref msg, MouseButton.Middle,
+                        MouseButtonEvent.DoubleClick);
+                    break;
+                }
+                case WM.XBUTTONUP:
+                {
+                    MessageHandlers.ProcessMouseButtonEvent(this, ref msg, MouseButton.Other, MouseButtonEvent.Up);
+                    break;
+                }
+                case WM.XBUTTONDOWN:
+                {
+                    MessageHandlers.ProcessMouseButtonEvent(this, ref msg, MouseButton.Other, MouseButtonEvent.Down);
+                    break;
+                }
+                case WM.XBUTTONDBLCLK:
+                {
+                    MessageHandlers.ProcessMouseButtonEvent(this, ref msg, MouseButton.Other,
+                        MouseButtonEvent.DoubleClick);
+                    break;
+                }
+                case WM.MOUSEACTIVATE:
+                {
+                    MessageHandlers.ProcessMouseActivate(this, ref msg);
+                    break;
+                }
+                case WM.MOUSEHOVER:
+                {
+                    MessageHandlers.ProcessMouseHover(this, ref msg);
+                    break;
+                }
+                case WM.MOUSEWHEEL:
+                {
+                    MessageHandlers.ProcessMouseWheel(this, ref msg, Orientation.Vertical);
+                    break;
+                }
+                case WM.MOUSEHWHEEL:
+                {
+                    MessageHandlers.ProcessMouseWheel(this, ref msg, Orientation.Horizontal);
+                    break;
+                }
+                case WM.MOUSELEAVE:
+                {
+                    MessageHandlers.ProcessMouseLeave(this, ref msg);
+                    break;
+                }
             }
             base.OnMessage(ref msg);
         }
@@ -94,6 +206,7 @@ namespace WinApi.XWin
         protected virtual void OnPaint(ref WindowMessage msg, IntPtr hdc) {}
         protected virtual void OnDisplayChange(ref WindowMessage msg, uint imageDepthBitsPerPixel, ref Size size) {}
         protected virtual void OnActivateApp(ref WindowMessage msg, bool isActive, long oppositeThreadId) {}
+        protected virtual void OnMouseMove(ref WindowMessage msg, ref Point point, MouseStateFlags flags) {}
 
         public static class MessageHandlers
         {
@@ -184,14 +297,58 @@ namespace WinApi.XWin
             public static void ProcessMouseMove(WindowBase windowBase, ref WindowMessage msg)
             {
                 Point point;
-                msg.LParam.BreakInt32Into16(out point.Y, out point.X);
-                windowBase.OnMouseMove(ref msg, ref point, (MouseMoveFlags)msg.WParam.ToSafeInt32());
+                msg.LParam.BreakSafeInt32To16Signed(out point.Y, out point.X);
+                var flags = (MouseStateFlags) msg.WParam.ToSafeInt32();
+                windowBase.OnMouseMove(ref msg, ref point, flags);
             }
-        }
 
-        protected virtual void OnMouseMove(ref WindowMessage msg, ref Point point, MouseMoveFlags flags)
-        {
-            
+            public static void ProcessMouseButtonEvent(WindowBase windowBase, ref WindowMessage msg, MouseButton button,
+                MouseButtonEvent eventType)
+            {
+                Point point;
+                msg.LParam.BreakSafeInt32To16Signed(out point.Y, out point.X);
+                var wParam = msg.WParam.ToSafeInt32();
+                var flags = (MouseStateFlags) wParam.Low();
+                if (button == MouseButton.Other)
+                {
+                    var xButton = (MouseXButtonFlag) wParam.High();
+                }
+            }
+
+            public static void ProcessMouseActivate(WindowBase windowBase, ref WindowMessage msg)
+            {
+                var activeTopLevelParentWindow = msg.WParam.ToSafeInt32();
+                var lParam = msg.LParam.ToSafeInt32();
+                var hitTestResult = (HitTestResult) lParam.Low();
+                var messageId = lParam.High();
+                MouseActivationResult result;
+            }
+
+            public static void ProcessMouseHover(WindowBase windowBase, ref WindowMessage msg)
+            {
+                Point point;
+                msg.LParam.BreakSafeInt32To16Signed(out point.Y, out point.X);
+                var flags = (MouseStateFlags) msg.WParam.ToSafeInt32();
+            }
+
+            public static void ProcessMouseLeave(WindowBase windowBase, ref WindowMessage msg)
+            {
+                // Nothing data here.
+            }
+
+            public static void ProcessMouseWheel(WindowBase windowBase, ref WindowMessage msg,
+                Orientation wheelOrientation)
+            {
+                Point point;
+                msg.LParam.BreakSafeInt32To16Signed(out point.Y, out point.X);
+                var wParam = msg.WParam.ToSafeInt32();
+                // Multiple or divisons of (WHEEL_DELTA = 120)
+                var wheelDelta = wParam.High();
+                if (wheelOrientation == Orientation.Vertical)
+                {
+                    var flags = (MouseStateFlags) wParam.Low();
+                }
+            }
         }
     }
 
