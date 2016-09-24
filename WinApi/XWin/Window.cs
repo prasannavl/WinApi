79656a198b01c9aa38c0139294c641fa96955b7b
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-09-24 22:00:34 +0530

    add: default factories to Window

--- WinApi/XWin/Window.cs
+++ WinApi/XWin/Window.cs
@@ -13,14 +13,30 @@ namespace WinApi.XWin
     public class Window : EventedWindowCore, IConstructionParamsProvider
     {
         public static Lazy<WindowFactory> ClassFactory = new Lazy<WindowFactory>(() => WindowFactory.Create());
-
-        public static Window Create()
-        {
-            var factory = ClassFactory.Value;
-            return factory.CreateWindow(() => new Window());
-        }
+        protected Window() {}
 
         IConstructionParams IConstructionParamsProvider.GetConstructionParams() => new FrameWindowConstructionParams();
-        protected Window() {}
+
+        public static Window Create(string text = null,
+            WindowStyles? styles = null,
+            WindowExStyles? exStyles = null, int? x = null, int? y = null,
+            int? width = null, int? height = null, IntPtr? hParent = null, IntPtr? hMenu = null,
+            WindowFactory factory = null)
+        {
+            return (factory ?? ClassFactory.Value).CreateWindow(() => new Window(), text, styles, exStyles, x, y, width,
+                height, hParent, hMenu);
+        }
+
+        public static TWindow Create<TWindow>(string text = null,
+            WindowStyles? styles = null,
+            WindowExStyles? exStyles = null, int? x = null, int? y = null,
+            int? width = null, int? height = null, IntPtr? hParent = null, IntPtr? hMenu = null,
+            WindowFactory factory = null)
+            where TWindow : WindowCore, new()
+        {
+            return (factory ?? ClassFactory.Value).CreateWindow(() => new TWindow(), null, text, styles, exStyles, x, y,
+                width, height, hParent,
+                hMenu);
+        }
     }
 }
