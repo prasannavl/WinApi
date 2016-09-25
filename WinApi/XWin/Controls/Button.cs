9f9fea9eefde9988a76e46a4ffa4328c1d675df5
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-09-25 15:46:13 +0530

    add: button control

---
+++ WinApi/XWin/Controls/Button.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinApi.User32;
+
+namespace WinApi.XWin.Controls
+{
+    public class Button : EventedWindowCore, IConstructionParamsProvider
+    {
+        public static Lazy<WindowFactory> ClassFactory = new Lazy<WindowFactory>(() => WindowFactory.CreateForExistingClass("button"));
+        protected Button() { }
+
+        IConstructionParams IConstructionParamsProvider.GetConstructionParams() => new ButtonConstructionParams();
+
+        public static Button Create(string text = null,
+            WindowStyles? styles = null,
+            WindowExStyles? exStyles = null, int? x = null, int? y = null,
+            int? width = null, int? height = null, IntPtr? hParent = null, IntPtr? hMenu = null,
+            WindowFactory factory = null)
+        {
+            return (factory ?? ClassFactory.Value).CreateWindow(() => new Button(), text, styles, exStyles, x, y, width,
+                height, hParent, hMenu);
+        }
+
+        public class ButtonConstructionParams : VisibleChildConstructionParams
+        {
+            public override int Width => 200;
+            public override int Height => 35;
+            public override WindowExStyles ExStyles => 0;
+        }
+    }
+}
