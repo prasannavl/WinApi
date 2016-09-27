4c7a1b98a6208fa20e709f6bbe6648e04eee6637
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-09-27 20:32:18 +0530

    add: gdi32 stock objects

--- WinApi/Gdi32/Types.cs
+++ WinApi/Gdi32/Types.cs
@@ -216,6 +216,35 @@ namespace WinApi.Gdi32
         DIB_PAL_COLORS = 1 /* color table in palette indices */
     }
 
+    public enum StockBrush
+    {
+        WHITE_BRUSH = 0,
+        LTGRAY_BRUSH = 1,
+        GRAY_BRUSH = 2,
+        DKGRAY_BRUSH = 3,
+        BLACK_BRUSH = 4,
+        NULL_BRUSH = 5,
+        HOLLOW_BRUSH = NULL_BRUSH,
+    }
+
+    public enum StockPen
+    {
+        WHITE_PEN = 6,
+        BLACK_PEN = 7,
+        NULL_PEN = 8,
+    }
+
+    public enum StockFont
+    {
+        OEM_FIXED_FONT = 10,
+        ANSI_FIXED_FONT = 11,
+        ANSI_VAR_FONT = 12,
+        SYSTEM_FONT = 13,
+        DEVICE_DEFAULT_FONT = 14,
+        SYSTEM_FIXED_FONT = 16,
+        DEFAULT_GUI_FONT = 17,
+    }
+
     [Flags]
     public enum BitBltFlags
     {
