28c7a4d57753737f17667bf41528eda90e676956
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-10-07 06:43:28 +0530

    change: update Skia sample

--- Samples/Sample.Skia/Program.cs
+++ Samples/Sample.Skia/Program.cs
@@ -1,23 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime.ExceptionServices;
+using System.Configuration;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 using SkiaSharp;
-using WinApi;
-using WinApi.Core;
-using WinApi.Kernel32;
-using WinApi.User32;
-using WinApi.User32.Experimental;
-using WinApi.XWin;
-using WinApi.Desktop;
 using WinApi.DwmApi;
+using WinApi.User32;
 using WinApi.UxTheme;
-using WinApi.XWin.Helpers;
+using System.Linq;
+using WinApi.Desktop;
+using WinApi.Extensions;
+using WinApi.Gdi32;
+using WinApi.Windows;
+using WinApi.Windows.Helpers;
 
 namespace Sample.Skia
 {
@@ -28,9 +22,9 @@ namespace Sample.Skia
             try
             {
                 ApplicationHelpers.SetupDefaultExceptionHandlers();
-                // Using it without a dependency on WinApi.XWin.Controls
-                var factory = WindowFactory.Create();
-                using (var win = factory.CreateWindow(() => new SkiaWindow(), "Hello", 
+                // Using it without a dependency on WinApi.Windows.Controls
+                var factory = WindowFactory.Create(hBgBrush: IntPtr.Zero);
+                using (var win = factory.CreateWindow(() => new SkiaWindow(), "Hello",
                     constructionParams: new FrameWindowConstructionParams()))
                 {
                     win.Show();
@@ -49,8 +43,16 @@ namespace Sample.Skia
     {
         protected override void OnSkiaPaint(SKSurface surface)
         {
+            var windowRect = GetWindowRect();
+            var clientRect = new Rectangle(windowRect.Width, windowRect.Height);
             var canvas = surface.Canvas;
-            canvas.Clear(new SKColor(70, 120, 110, 200));
+            canvas.Clear(new SKColor(120, 170, 140, 255));
+            var textPainter = new SKPaint() {TextSize = 35, IsAntialias = true};
+            var str = "Hello there!";
+            var textBounds = new SKRect();
+            var m = textPainter.MeasureText(str, ref textBounds);
+            canvas.DrawText(str, (clientRect.Width - textBounds.Width)/2, (clientRect.Height - textBounds.Height)/2,
+                textPainter);
             base.OnSkiaPaint(surface);
         }
     }
