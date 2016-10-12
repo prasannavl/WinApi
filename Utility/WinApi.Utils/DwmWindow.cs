feb1edbc25d53633ff3473dd3b10c0e0782522fa
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-10-12 22:13:43 +0530

    add: DwmWindowHelper

--- Utility/WinApi.Utils/DwmWindow.cs
+++ Utility/WinApi.Utils/DwmWindow.cs
@@ -10,172 +10,44 @@ namespace WinApi.Utils
 {
     public class DwmWindow : EventedWindowCore
     {
-        protected Rectangle DwmFrameInsetRect;
-        protected Rectangle DwmFrameOutsetRect;
-        private bool m_isFirstNcCalcDone;
+        private readonly DwmWindowHelper m_dwmHelper;
 
-        protected bool DwmDrawCaptionIcon { get; set; }
-        protected bool DwmDrawCaptionTitle { get; set; }
-        protected bool DwmBlurBehind { get; set; }
-        protected bool DwmAllowSystemMenu { get; set; }
-
-        protected virtual int GetSystemTopBorderHeight()
+        public DwmWindow()
         {
-            return User32Methods.GetSystemMetrics(SystemMetrics.SM_CYBORDER);
+            m_dwmHelper = new DwmWindowHelper(this);
         }
+
+        protected bool DrawCaptionIcon { get; set; }
+        protected bool DrawCaptionTitle { get; set; }
+        protected bool AllowSystemMenu { get; set; }
 
         protected override CreateWindowResult OnCreate(ref WindowMessage msg, ref CreateStruct createStruct)
         {
-            SetupWindowThemeAttributes();
-            CalculateNcOutset(ref DwmFrameOutsetRect);
-            RedrawFrame();
+            m_dwmHelper.OnCreate(DrawCaptionIcon, DrawCaptionTitle, AllowSystemMenu);
             return base.OnCreate(ref msg, ref createStruct);
-        }
-
-        protected void CalculateNcOutset(ref Rectangle outsetRect)
-        {
-            User32Methods.AdjustWindowRectEx(ref outsetRect, GetStyles(), false, GetExStyles());
-        }
-
-        protected virtual Margins GetDwmMargins()
-        {
-            return new Margins(DwmFrameInsetRect.Left, DwmFrameInsetRect.Right,
-                // -DwmFrameInsetRect.Top
-                // Use above instead to add Dwm top frame extension without accounting for the current offset.
-                // DwmFrameInsetRect.Top should be ideally the negative value of caption area height including borders. 
-                // So offseting this way, instead of subtracting, removes the title bar entirely,
-                // with the exception of top border (which is a part of a client area now).
-                GetSystemTopBorderHeight() + DwmFrameInsetRect.Top,
-                DwmFrameInsetRect.Bottom);
-        }
-
-        protected virtual void SetupWindowThemeAttributes()
-        {
-            WindowThemeNcAttributeFlags flags = 0;
-            if (!DwmDrawCaptionIcon)
-                flags |= WindowThemeNcAttributeFlags.WTNCA_NODRAWICON;
-            if (!DwmDrawCaptionTitle)
-                flags |= WindowThemeNcAttributeFlags.WTNCA_NODRAWCAPTION;
-            if (!DwmAllowSystemMenu)
-                flags |= WindowThemeNcAttributeFlags.WTNCA_NOSYSMENU;
-            UxThemeHelpers.SetWindowThemeNonClientAttributes(Handle,
-                WindowThemeNcAttributeFlags.WTNCA_VALIDBITS & ~WindowThemeNcAttributeFlags.WTNCA_NOMIRRORHELP, flags);
-        }
-
-        protected virtual void SetupDwm()
-        {
-            var dwmMargins = GetDwmMargins();
-            DwmApiMethods.DwmExtendFrameIntoClientArea(Handle, ref dwmMargins);
-            var policy = (int) DwmNCRenderingPolicy.DWMNCRP_ENABLED;
-            DwmApiHelpers.DwmSetWindowAttribute(Handle, DwmWindowAttributeType.DWMWA_NCRENDERING_POLICY,
-                ref policy);
-            if (DwmBlurBehind)
-                User32ExperimentalHelpers.EnableBlurBehind(Handle);
         }
 
         protected override void OnActivate(ref WindowMessage msg, WindowActivateFlag flag, bool isMinimized,
             IntPtr oppositeHwnd)
         {
-            SetupDwm();
+            m_dwmHelper.ApplyDwmConfig();
             base.OnActivate(ref msg, flag, isMinimized, oppositeHwnd);
         }
 
         protected override WindowViewRegionFlags OnNcCalcSize(ref WindowMessage msg, bool shouldCalcValidRects,
             ref NcCalcSizeParams ncCalcSizeParams)
         {
-            if (!m_isFirstNcCalcDone)
-            {
-                // Use default processing the very first time to ensure compatibility - cascade
-                // and stack windows don't work otherwise.
-                m_isFirstNcCalcDone = true;
-                return base.OnNcCalcSize(ref msg, shouldCalcValidRects, ref ncCalcSizeParams);
-            }
-            ncCalcSizeParams.Region.Output.TargetClientRect.Top += 0;
-            ncCalcSizeParams.Region.Output.TargetClientRect.Bottom -= DwmFrameOutsetRect.Bottom;
-            ncCalcSizeParams.Region.Output.TargetClientRect.Left -= DwmFrameOutsetRect.Left;
-            ncCalcSizeParams.Region.Output.TargetClientRect.Right -= DwmFrameOutsetRect.Right;
-            return 0;
-        }
-
-        protected virtual void GetFrameEdgeRect(out Rectangle rect)
-        {
-            GetClientRect(out rect);
-            User32Helpers.MapWindowPoints(Handle, IntPtr.Zero, ref rect);
-            rect.Left += DwmFrameOutsetRect.Left;
-            rect.Right += DwmFrameOutsetRect.Right;
-            rect.Bottom += DwmFrameOutsetRect.Bottom;
-        }
-
-        public static void GetFrameEdgeSizerWidth(out CartesianWidth width)
-        {
-            var borderPadding = User32Methods.GetSystemMetrics(SystemMetrics.SM_CXPADDEDBORDER);
-            width.X = User32Methods.GetSystemMetrics(SystemMetrics.SM_CXSIZEFRAME) + borderPadding;
-            width.Y = User32Methods.GetSystemMetrics(SystemMetrics.SM_CYSIZEFRAME) + borderPadding;
-        }
-
-        protected static HitTestResult FrameHitTest(ref Point point, ref Rectangle frameEdgeRect,
-            ref CartesianWidth frameEdgeSizerWidth)
-        {
-            var x = point.X;
-            var y = point.Y;
-
-            var topEdge = frameEdgeRect.Top;
-            var leftEdge = frameEdgeRect.Left;
-            var rightEdge = frameEdgeRect.Right;
-            var bottomEdge = frameEdgeRect.Bottom;
-
-            var sizerXWidth = frameEdgeSizerWidth.X;
-            var sizerYWidth = frameEdgeSizerWidth.Y;
-
-            if (y < topEdge + sizerYWidth)
-            {
-                // Inside the top sizing area
-                if (x < leftEdge + 2*sizerXWidth)
-                {
-                    return HitTestResult.HTTOPLEFT;
-                }
-                if (x >= rightEdge - 2*sizerXWidth)
-                {
-                    return HitTestResult.HTTOPRIGHT;
-                }
-                return HitTestResult.HTTOP;
-            }
-            if (y < bottomEdge - sizerYWidth)
-            {
-                // Inside the client area
-                if (x < leftEdge + sizerXWidth)
-                {
-                    return HitTestResult.HTLEFT;
-                }
-                if (x >= rightEdge - sizerXWidth)
-                {
-                    return HitTestResult.HTRIGHT;
-                }
-                return HitTestResult.HTCLIENT;
-            }
-            // Inside the bottom area
-            if (x < leftEdge + 2*sizerXWidth)
-            {
-                return HitTestResult.HTBOTTOMLEFT;
-            }
-            if (x >= rightEdge - 2*sizerXWidth)
-            {
-                return HitTestResult.HTBOTTOMRIGHT;
-            }
-            return HitTestResult.HTBOTTOM;
+            if (m_dwmHelper.TryHandleNcCalcSize(ref ncCalcSizeParams))
+                return (WindowViewRegionFlags) msg.Result;
+            return base.OnNcCalcSize(ref msg, shouldCalcValidRects, ref ncCalcSizeParams);
         }
 
         protected override HitTestResult OnHitTest(ref WindowMessage msg, ref Point point)
         {
-            Rectangle frameEdge;
-            CartesianWidth frameSizerWidth;
-            GetFrameEdgeRect(out frameEdge);
-            GetFrameEdgeSizerWidth(out frameSizerWidth);
-            var captionHeight = -DwmFrameOutsetRect.Top + DwmFrameInsetRect.Top;
-            var res = FrameHitTest(ref point, ref frameEdge, ref frameSizerWidth);
+            var res = m_dwmHelper.HitTest(ref point);
             if (res == HitTestResult.HTCLIENT)
             {
-                if (point.Y < frameEdge.Top + captionHeight)
+                if (point.Y < m_dwmHelper.GetCaptionHeight())
                 {
                     // Also do button hit testing
                     return HitTestResult.HTCAPTION;
---
+++ Utility/WinApi.Utils/DwmWindowHelper.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using WinApi.Core;
+using WinApi.DwmApi;
+using WinApi.User32;
+using WinApi.User32.Experimental;
+using WinApi.UxTheme;
+using WinApi.Windows;
+
+namespace WinApi.Utils
+{
+    public sealed class DwmWindowHelper : DwmWindowHelperCore
+    {
+        public DwmWindowHelper(EventedWindowCore window) : base(window) {}
+    }
+
+    public class DwmWindowHelperCore
+    {
+        private readonly EventedWindowCore m_window;
+        public bool BlurBehindEnabled;
+
+        private bool m_isFirstNcCalcDone;
+        public Rectangle NcOutsetRect;
+        public Rectangle Padding;
+
+        public DwmWindowHelperCore(EventedWindowCore window)
+        {
+            m_window = window;
+        }
+
+        public static int GetSystemTopBorderHeight()
+        {
+            return User32Methods.GetSystemMetrics(SystemMetrics.SM_CYBORDER);
+        }
+
+        public static void SetWindowThemeAttributes(IntPtr handle, bool drawCaptionIcon, bool drawCaptionTitle,
+            bool allowSystemMenu)
+        {
+            WindowThemeNcAttributeFlags flags = 0;
+            if (!drawCaptionIcon)
+                flags |= WindowThemeNcAttributeFlags.WTNCA_NODRAWICON;
+            if (!drawCaptionTitle)
+                flags |= WindowThemeNcAttributeFlags.WTNCA_NODRAWCAPTION;
+            if (!allowSystemMenu)
+                flags |= WindowThemeNcAttributeFlags.WTNCA_NOSYSMENU;
+            UxThemeHelpers.SetWindowThemeNonClientAttributes(handle,
+                WindowThemeNcAttributeFlags.WTNCA_VALIDBITS & ~WindowThemeNcAttributeFlags.WTNCA_NOMIRRORHELP, flags);
+        }
+
+        public static Rectangle GetSystemNcOutsetThickness(EventedWindowCore window)
+        {
+            var rect = new Rectangle();
+            User32Methods.AdjustWindowRectEx(ref rect, window.GetStyles(), false, window.GetExStyles());
+            return rect;
+        }
+
+        public static void GetSystemSizingFrameThickness(out CartesianValue width)
+        {
+            var borderPadding = User32Methods.GetSystemMetrics(SystemMetrics.SM_CXPADDEDBORDER);
+            width.X = User32Methods.GetSystemMetrics(SystemMetrics.SM_CXSIZEFRAME) + borderPadding;
+            width.Y = User32Methods.GetSystemMetrics(SystemMetrics.SM_CYSIZEFRAME) + borderPadding;
+        }
+
+        public static HitTestResult FrameHitTest(ref Point point, ref Rectangle frameThickness,
+            ref CartesianValue sizingFrameThickness)
+        {
+            var x = point.X;
+            var y = point.Y;
+
+            var topEdge = frameThickness.Top;
+            var leftEdge = frameThickness.Left;
+            var rightEdge = frameThickness.Right;
+            var bottomEdge = frameThickness.Bottom;
+
+            var sizerXWidth = sizingFrameThickness.X;
+            var sizerYWidth = sizingFrameThickness.Y;
+
+            if (y < topEdge + sizerYWidth)
+            {
+                // Inside the top sizing area
+                if (x < leftEdge + 2 * sizerXWidth)
+                {
+                    return HitTestResult.HTTOPLEFT;
+                }
+                if (x >= rightEdge - 2 * sizerXWidth)
+                {
+                    return HitTestResult.HTTOPRIGHT;
+                }
+                return HitTestResult.HTTOP;
+            }
+            if (y < bottomEdge - sizerYWidth)
+            {
+                // Inside the client area
+                if (x < leftEdge + sizerXWidth)
+                {
+                    return HitTestResult.HTLEFT;
+                }
+                if (x >= rightEdge - sizerXWidth)
+                {
+                    return HitTestResult.HTRIGHT;
+                }
+                return HitTestResult.HTCLIENT;
+            }
+            // Inside the bottom area
+            if (x < leftEdge + 2 * sizerXWidth)
+            {
+                return HitTestResult.HTBOTTOMLEFT;
+            }
+            if (x >= rightEdge - 2 * sizerXWidth)
+            {
+                return HitTestResult.HTBOTTOMRIGHT;
+            }
+            return HitTestResult.HTBOTTOM;
+        }
+
+        public virtual int GetTopBorderHeight()
+        {
+            return GetSystemTopBorderHeight();
+        }
+
+        protected virtual Margins GetDwmMargins()
+        {
+            return new Margins(Padding.Left, Padding.Right,
+                // -Padding.Top
+                // Use above instead to add Dwm top frame extension without accounting for the current offset.
+                // Padding.Top should be ideally the negative value of caption area height including borders. 
+                // So offseting this way, instead of subtracting, removes the title bar entirely,
+                // with the exception of top border (which is a part of a client area now).
+                GetTopBorderHeight() + Padding.Top,
+                Padding.Bottom);
+        }
+
+        public virtual void GetSizingFrameThickness(out CartesianValue width)
+        {
+            GetSystemSizingFrameThickness(out width);
+        }
+
+        public virtual void GetFrameThickness(out Rectangle rect)
+        {
+            m_window.GetClientRect(out rect);
+            User32Helpers.MapWindowPoints(m_window.Handle, IntPtr.Zero, ref rect);
+            rect.Left += NcOutsetRect.Left;
+            rect.Right += NcOutsetRect.Right;
+            rect.Bottom += NcOutsetRect.Bottom;
+        }
+
+        public virtual void ApplyDwmConfig()
+        {
+            var window = m_window;
+            var dwmMargins = GetDwmMargins();
+            DwmApiMethods.DwmExtendFrameIntoClientArea(window.Handle, ref dwmMargins);
+            var policy = (int)DwmNCRenderingPolicy.DWMNCRP_ENABLED;
+            DwmApiHelpers.DwmSetWindowAttribute(window.Handle, DwmWindowAttributeType.DWMWA_NCRENDERING_POLICY,
+                ref policy);
+            if (BlurBehindEnabled)
+                User32ExperimentalHelpers.EnableBlurBehind(window.Handle);
+        }
+
+        public virtual void OnCreate(bool drawCaptionIcon, bool drawCaptionTitle,
+            bool allowSystemMenu)
+        {
+            var window = m_window;
+            SetWindowThemeAttributes(window.Handle, drawCaptionIcon, drawCaptionTitle, allowSystemMenu);
+            NcOutsetRect = GetSystemNcOutsetThickness(window);
+            window.RedrawFrame();
+        }
+
+        public virtual HitTestResult HitTest(ref Point point)
+        {
+            Rectangle frameThickness;
+            CartesianValue sizingFrameThickness;
+            GetFrameThickness(out frameThickness);
+            GetSizingFrameThickness(out sizingFrameThickness);
+            return FrameHitTest(ref point, ref frameThickness, ref sizingFrameThickness);
+        }
+
+        public virtual int GetCaptionHeight()
+        {
+            return -NcOutsetRect.Top + Padding.Top + GetTopBorderHeight();
+        }
+
+        public virtual bool TryHandleNcCalcSize(ref NcCalcSizeParams ncCalcSizeParams)
+        {
+            if (!m_isFirstNcCalcDone)
+            {
+                // Use default processing the very first time to ensure compatibility - cascade
+                // and stack windows don't work otherwise.
+                m_isFirstNcCalcDone = true;
+                return false;
+            }
+            ncCalcSizeParams.Region.Output.TargetClientRect.Top += 0;
+            ncCalcSizeParams.Region.Output.TargetClientRect.Bottom -= NcOutsetRect.Bottom;
+            ncCalcSizeParams.Region.Output.TargetClientRect.Left -= NcOutsetRect.Left;
+            ncCalcSizeParams.Region.Output.TargetClientRect.Right -= NcOutsetRect.Right;
+            return true;
+        }
+    }
+
+}
