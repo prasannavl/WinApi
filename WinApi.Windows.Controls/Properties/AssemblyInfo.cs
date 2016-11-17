ba04462de0113fb28df70476ee4948950cc624ba
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-11-18 04:27:02 +0530

    package: version 3

--- WinApi.Windows.Controls/Properties/AssemblyInfo.cs
+++ WinApi.Windows.Controls/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.0")]
